3d70d2d903321f9bff59e77f41426a804c8cfa4e
Marina Novosad <marina-budnikova8@rambler.ru>
2021-12-30 17:52:33 +0300

    Chapter 6. Exercise 9.

---
+++ 06_control_statements_looping/exercises/calculate.c
@@ -0,0 +1,28 @@
+// Modify exercise 8 so that it uses a function to return the value of the 
+// calculation.
+#include <stdio.h>
+
+float calculate(float first_num, float next_num); 
+
+int main(void)
+{
+    float first_num, next_num;
+    int status;
+
+    printf("Please enter two floats ");
+    printf("(q to quit): ");
+    status = scanf("%f %f", &first_num, &next_num);
+    while (status == 2)
+    {
+        printf("The value of their difference divided by their product = %.2f\n", calculate(first_num, next_num));
+        printf("Please enter two floats (q to quit): ");
+        status = scanf("%f %f", &first_num, &next_num);
+    }
+    return 0;
+}
+
+float calculate(float first_num, float next_num)
+{
+    (first_num - next_num) / (first_num * next_num);
+    return (first_num - next_num) / (first_num * next_num);
+}
