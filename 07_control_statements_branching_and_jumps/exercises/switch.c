62c8af63be3703feafa5e9bb0830cf23ddc50501
Marina Novosad <marina-budnikova8@rambler.ru>
2022-03-09 19:35:56 +0300

    Chapter 7. Exercise 5. Replacements with Switch

---
+++ 07_control_statements_branching_and_jumps/exercises/switch.c
@@ -0,0 +1,31 @@
+// Redo exercise 4 using a switch.
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int point_count = 0;
+    int excl_mark_count = 0;
+
+    while ((ch = getchar()) != '#')
+    {
+        switch (ch)
+        {
+        case '.' :
+            putchar('!');
+            point_count++;
+            break;       
+        case '!' :
+            putchar('!');
+            putchar('!');
+            excl_mark_count++;
+            break;
+            default :
+            putchar(ch);
+        }
+    }
+    printf("\nThe point has been replaced %d times, exclamation mark has been "
+            "replaced %d times.\n", point_count, excl_mark_count);
+
+    return 0;
+}
