196c163bae72386f517ac174852bdb80049f536b
Marina Novosad <marina-budnikova8@rambler.ru>
2022-03-10 18:53:17 +0300

    Chapter 7. Exercise 6. Count EI

---
+++ 07_control_statements_branching_and_jumps/exercises/count_ei.c
@@ -0,0 +1,25 @@
+// Write a program that reads input up to # and reports the number of times 
+// that the sequence ei occurs.Note: The program will have to “remember” the 
+// preceding character as well as the current character. Test it with input 
+// such as Receive your eieio award.
+#include <stdio.h>
+
+int main(void)
+{
+    char ch;
+    int count = 0;
+
+    while ((ch = getchar()) != '#')
+    {
+        if (ch == 'e')
+        {    
+            ch = getchar(); 
+            if (ch == 'i')
+                count++;
+                continue;
+        }          
+    }
+    printf("The %d times the sequence 'ei' occurs.\n", count);
+
+    return 0;
+}
