98c47fa9978f4fd502ad87d3ade3c002650b7550
Marina Novosad <zmeika.marina@gmail.com>
2023-04-27 10:10:33 +0000

    Fixed some bugs

--- 08_character_input_output/exercises/guess.c
+++ 08_character_input_output/exercises/guess.c
@@ -3,7 +3,7 @@
 // whether the guess is high, low, or correct. If, say, the guess is low, have the next guess
 // be halfway between 50 and 100, that is 75. If that guess is high, let the next guess be
 // halfway between 75 and 50, and so on. Using this binary search strategy, the program
-// quickly zeros in on the correct answer, at least if the userdoes not cheat.
+// quickly zeros in on the correct answer, at least if the user does not cheat.
 #include <stdio.h>
 
 int main(void)
@@ -29,16 +29,15 @@ int main(void)
 	    max = mid;
             printf("Well, then, is it %d?\n", mid -= (max - min) / 2);
 	}
-	else if (response == 'y')
-	{
-            printf("I knew I could do it!\n");
+        else if (response == 'y')
+
 	    return 0;
-	}
 	else
-	    printf("Sorry, I understand only l,h or y. \n");
+	    printf("Sorry, I understand only l, h or y. \n");
         while (getchar() != '\n')
        	    continue;       
     }	
-	   
+    printf("I knew I could do it!\n");
+
     return 0;
 }
