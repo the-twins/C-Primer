5d140136dc8c329953464e1911966c9d8501b6fc
Marina Novosad <zmeika.marina@gmail.com>
2023-09-13 15:27:22 +0000

    Chapter 11. Listings.

---
+++ 11_strings/examples/addresses.c
@@ -0,0 +1,17 @@
+// addresses.c -- adresses of strings
+#define MSG "I'm special."
+
+#include <stdio.h>
+
+int main(void)
+{
+    char ar[] = MSG;
+    const char *pt = MSG;
+    printf("address of \"I'm special\": %p \n", "I'm special");
+    printf("              address ar: %p\n", ar);
+    printf("              address pt: %p\n", pt);
+    printf("         address of MSG: %p\n", MSG);
+    printf("address of \"I'm special\": %p \n", "I'm special");
+
+    return 0;
+}
