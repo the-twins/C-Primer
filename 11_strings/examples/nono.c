18393b57ed7a0ed9e6142b950af824ec5abecdc7
Marina Novosad <zmeika.marina@gmail.com>
2023-09-18 15:19:46 +0000

    Chapter 11. Listings.

---
+++ 11_strings/examples/nono.c
@@ -0,0 +1,13 @@
+// nono.c -- no!
+#include <stdio.h>
+
+int main(void)
+{
+    char side_a[] = "Side A";
+    char dont[] = {'W', 'O', 'W','!'};
+    char side_b[] = "Side B";
+
+    puts(dont);    //dont is not a string
+
+    return 0;
+}
