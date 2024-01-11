d0e20afe1c1dda872e7103b2e44b793f6a055cf9
Marina Novosad <zmeika.marina@gmail.com>
2024-01-11 15:11:26 +0000

    Fixed some bugs.

--- 12_storage_classes/exercises/ex_03.c
+++ 12_storage_classes/exercises/ex_03.c
@@ -6,35 +6,40 @@
 #include <stdlib.h>
 
 #define KM 100
-
+void set_mode (int * mode);
 void get_info(int mode, float * distance, float * fuel_com);
 void show_info(int mode, float distance, float fuel_com);
 
 int main(void)
 {
-    int f_mode;
-    int mode = 0;
+    int mode;
     float distance, fuel_com;
     printf("Enter 0 for metric mode, 1 for US mode: ");
-    scanf("%d", &f_mode);
-    if(f_mode >= 2)
-        printf("Invalid mode specified. Mode %d used.\n", mode);
-    else
-        mode = f_mode;
+    scanf("%d", &mode);
+    set_mode(&mode);
     while(mode >= 0)
     {
 	get_info(mode, &distance, &fuel_com);
 	show_info(mode, distance, fuel_com);
         printf("Enter 0 for metric mode, 1 for US mode");
         printf(" (-1 to quit): ");
-        scanf("%d", &f_mode);
-	if(f_mode >= 2)
-	    printf("Invalid mode specified. Mode %d used.\n", mode);
-        else
-	    mode = f_mode;
+        scanf("%d", &mode);
+	set_mode(&mode);
      }
      printf("Done.\n");
      return 0;
+}
+
+void set_mode(int * mode)
+{
+    int n_mode;
+    if(*mode >= 2)
+    {
+        printf("Invalid mode specified. Mode %d used.\n", n_mode);
+        *mode = n_mode;
+    }
+    else
+        n_mode = *mode;
 }
 
 void get_info(int mode, float * distance, float * fuel_com)
@@ -53,8 +58,6 @@ void get_info(int mode, float * distance, float * fuel_com)
         printf("Enter fuel consumed in gallons: ");
         scanf("%f", fuel_com);
     }
-    if(mode == -1)
-        exit(EXIT_FAILURE);
 }
 
 void show_info(int mode, float distance, float fuel_com)
@@ -63,6 +66,4 @@ void show_info(int mode, float distance, float fuel_com)
         printf("Fuel consumption is %.2f liters per %d km.\n", KM * fuel_com / distance, KM);
     if(mode == 1)
         printf("Fuel consumption is %.2f miles per gallon.\n", distance / fuel_com);
-    if(mode == -1)
-        exit(EXIT_FAILURE);
 }
