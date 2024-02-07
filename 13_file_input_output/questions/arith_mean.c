203471e490bd2fc69dc6a36f40d02c6cfe9b239d
Marina Novosad <zmeika.marina@gmail.com>
2024-02-07 14:49:39 +0000

    Chapter 13. Question 04. Arithmetic mean.

---
+++ 13_file_input_output/questions/arith_mean.c
@@ -0,0 +1,35 @@
+// Write a program that takes zero command-line arguments or one command-line
+// argument. If there is one argument, it is interpreted as the name of a file. If there is no
+// argument, the standard input (stdin) is to be used for input. Assume that the input
+// consists entirely of floating-point numbers. Have the program calculate and report the
+// arithmetic mean (the average) of the input numbers.
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[])
+{
+    FILE * fp;
+    double numb;
+    double sum = 0.0;
+    int count = 0;
+    if(argc == 1)
+    {
+        fp = stdin;
+        printf("Enter floatinf-point numbers to calculate the arithmetic mean:\n");
+        printf("To complete, enter # at the beginning of the line.\n");
+    }
+    else if(argc == 2)
+        if((fp = fopen(argv[1], "r")) == NULL)
+    {
+        printf("Could not open %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while(fscanf(fp,"%lf", &numb) == 1 && (numb != '#'))
+    {
+        sum += numb;
+        count++;
+    }
+    printf("The arithmetic mean of the input numbers = %lf\n", sum / count);
+
+    return 0;
+}
