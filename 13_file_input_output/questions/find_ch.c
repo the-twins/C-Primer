e8dc4832964fc39c943821ea6d00912420e4db1f
Marina Novosad <zmeika.marina@gmail.com>
2024-02-08 14:50:24 +0000

    Chapter 13. Question 05. Strchr.

---
+++ 13_file_input_output/questions/find_ch.c
@@ -0,0 +1,29 @@
+// Write a program that takes two command-line arguments. The first is a character, and
+// the second is a filename. The program should print only those lines in the file containing
+// the given character.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define SIZE 256
+
+int main(int argc, char * argv[])
+{
+    char * ch;
+    FILE * fp;
+    char str[SIZE];
+    if(argc < 2)
+        exit(EXIT_FAILURE);
+    if((fp = fopen(argv[2], "r")) == NULL)
+        exit(EXIT_FAILURE);
+    while(fgets(str, SIZE, fp) != NULL)
+    {
+        ch = strchr(str, argv[1][0]);
+	if(ch)
+            puts(str);
+        else
+            continue;
+    }
+
+    return 0;
+}
