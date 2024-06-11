f5e76681fad9ad801791fa2c2af87d5ef1fe4519
Marina Novosad <zmeika.marina@gmail.com>
2024-06-11 14:28:58 +0000

    Chapter 14. Exercise 07. Books save and delete.

---
+++ 14_structures/exercises/booksave_delete.c
@@ -0,0 +1,121 @@
+// Modify Listing 14.14 so that as each record is read from the file and shown to you, you
+// are given the chance to delete the record or to modify its contents. If you delete the
+// record, use the vacated array position for the next record to be read. To allow changing
+// the existing contents, you'll need to use the "r+b" mode instead of the "a+b" mode, and
+// you'll have to pay more attention to positioning the file pointer so that appended records
+// don't overwrite existing records. It's simplest to make all changes in the data stored in
+// program memory and then write the final set of information to the file. One approach to
+// keeping track is to add a member to the book structure that indicates whether it is to be
+// deleted.
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAXTITL 40
+#define MAXAUTL 40
+#define MAXBKS 10
+
+char * s_gets(char * st, int n);
+
+struct book {
+    char title[MAXTITL];
+    char author[MAXAUTL];
+    float value;
+};
+
+int main (void)
+{
+    struct book library[MAXBKS];
+    int count = 0;
+    int option = 0;
+    int index;
+    FILE * pbooks;
+    int size = sizeof(struct book);
+
+    pbooks = fopen("book.dat", "r+b");
+    rewind(pbooks);
+    while(count < MAXBKS && fread(&library[count], size, 1, pbooks) == 1)
+    {
+        if(count == 0)
+            puts("Current contents of book.dat:");
+        printf("%s by %s: $%.2f\n", library[count].title,
+               library[count].author, library[count].value);
+        printf("Choose an option:\n1) Change string\n2) Delete string\n3) Continue\n");
+        scanf("%d", &option);
+        if(option == 1)
+        {
+            puts("Enter the book titles");
+	    while(getchar() != '\n');
+            s_gets(library[count].title, MAXTITL);
+            puts("Enter the author");
+            s_gets(library[count].author, MAXAUTL);
+            puts("Enter the value");
+            scanf("%f", &library[count].value);
+            count++;
+        }
+        if(option == 2)
+            break;
+        if(option == 3)
+            count++;
+    }
+    fclose(pbooks);
+
+    if(count == MAXBKS)
+    {
+        fputs("The book.dat file is full.", stderr);
+        exit(2);
+    }
+    puts("Please add new book titles.");
+    puts("Press [enter] at the start of a line to stop.");
+    while(count < MAXBKS && s_gets(library[count].title, MAXTITL) != NULL
+          && library[count].title[0] != '\0')
+    {
+        puts("Now enter the author.");
+        s_gets(library[count].author, MAXAUTL);
+        puts("Nou enter the value.");
+        scanf("%f", &library[count].value);
+        while(getchar() != '\n')
+            continue;
+        count++;
+        if(count < MAXBKS)
+            puts("Enter the next title.");
+    }
+    if((pbooks = fopen("book.dat", "w+b")) == NULL)
+    {
+        fprintf(stderr,"Can't open file 'book.dat' fo write.\n");
+        EXIT_FAILURE;
+    }
+    if(count > 0)
+    {
+        fwrite(library, size, count, pbooks);
+        rewind(pbooks);
+        puts("Here is the list of your books:");
+        while(fread(library, size, 1, pbooks))
+            printf("%s by %s: $%.2f\n", library[0].title, library[0].author,  
+                   library[0].value);
+    }
+    else
+        puts("No books? Too bad.\n");
+    puts("Bye.\n");
+    fclose(pbooks);
+
+    return 0;
+}
+
+char * s_gets(char * st, int n)
+{
+    char * ret_val;
+    char * find;
+
+    ret_val = fgets(st, n, stdin);
+    if(ret_val)
+    {
+        find = strchr(st, '\n');
+        if(find)
+            *find = '\0';
+        else
+            while(getchar() != '\n')
+                continue;
+    }
+    return ret_val;
+}
